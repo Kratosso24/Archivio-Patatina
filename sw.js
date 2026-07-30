1d965ffcf40249757f5d605d82e077dbe54af4cb
Kratosso24 <davisalve03@gmail.com>
2026-07-31 01:30:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -19,6 +19,8 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return; // non intercettare chiamate esterne (es. API GitHub)
   event.respondWith(
     fetch(event.request)
       .then((res) => {
